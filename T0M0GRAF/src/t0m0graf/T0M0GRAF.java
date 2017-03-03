9d753fc8cc465903a5e8fd4333f3064c50c3d775
Anna <anusia1199@o2.pl>
2017-03-03 23:47:27 +0100

    Wczytywanie obrazu

--- T0M0GRAF/src/t0m0graf/T0M0GRAF.java
+++ T0M0GRAF/src/t0m0graf/T0M0GRAF.java
@@ -5,18 +5,72 @@
  */
 package t0m0graf;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.stream.Stream;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
 /**
  *
  * @author Ania
  */
-public class T0M0GRAF {
+public class T0M0GRAF extends JPanel {
+
+    /**
+     * Okienko
+     */
+    public static JFrame frame;
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-           
+        frame = new JFrame("Tomograf");
+        try {
+            T0M0GRAF tomograf = new T0M0GRAF();
+        } catch (IOException ex) {
+            Logger.getLogger(T0M0GRAF.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        frame.setVisible(true);
     }
-    
+
+    /**
+     * Tworzenie tomografu
+     *
+     * @throws IOException
+     */
+    public T0M0GRAF() throws IOException {
+        readIn(getClass().getResourceAsStream("obraz.bmp"));
+
+    }
+
+    /**
+     * Wczytywanie obrazu
+     *
+     * @param path sciezka do pliku z obrazem tomografii
+     */
+    public static void readIn(InputStream path) {
+        try {
+            JLabel label1 = new JLabel();
+            frame.add(label1);
+            BufferedImage bi;
+            bi = ImageIO.read(path);
+            ImageIcon imgIcon = new ImageIcon(bi);
+            label1.setIcon(imgIcon);
+        } catch (IOException ex) {
+            Logger.getLogger(T0M0GRAF.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
 }
