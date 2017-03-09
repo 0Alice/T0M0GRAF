a8a1127f39f3b1b8b2ff8f3b6e8af772e7a85552
Anna <anusia1199@o2.pl>
2017-03-09 10:58:10 +0100

    Sinogram

--- T0M0GRAF/src/t0m0graf/Picture.java
+++ T0M0GRAF/src/t0m0graf/Picture.java
@@ -5,6 +5,7 @@
  */
 package t0m0graf;
 
+import java.awt.Color;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
@@ -23,28 +24,9 @@ import java.awt.image.PixelGrabber;
  */
 public class Picture {
 
-    int off;
-    int[] pix;
-    int pixX;
-    int pixY;
-    int r;
-    /**
-     * Rozwarość stożka
-     */
-    int B = 1 / 2;
-    /**
-     * Ile punktów
-     */
-    int n;
-
-    /*
-    Rownanie parametryczne kola 
-    
-     */
     public Picture() {
         readIn(getClass().getResourceAsStream("obraz.bmp"));
-        //jak zadziaal to to 300 trzeba zmienic na szerokosc i wysokosc obrazu
-
+        //tu trzeba zmienic na choosera
     }
 
     /**
@@ -54,41 +36,14 @@ public class Picture {
      */
     public void readIn(InputStream path) {
         try {
+            BufferedImage bi;
             JLabel label1 = new JLabel();
             frame.add(label1);
-            BufferedImage bi;
             bi = ImageIO.read(path);
-            r = bi.getWidth();
-
             ImageIcon imgIcon = new ImageIcon(bi);
             label1.setIcon(imgIcon);
-
-            //to trzeba dostosowac
-            for (int i = 0; i < 360; i++) {
-                double help0 = i * Math.PI / 180;
-                Double EmiterX = Math.cos(help0) * r;
-                Double EmiterY = Math.sin(help0) * r;
-
-                for (int j= 0; j < n; j++) {
-                    double help = help0 + Math.PI - B / 2 * Math.PI + B * Math.PI * j / (n - 1);
-                    double DetektorX = Math.cos(help) * r;
-                    double DetektorY = Math.sin(help) * r;
-
-                    for (int k = 0; k < 10; k++) {
-                        
-                      //  PixelGrabber pixelGrab = new PixelGrabber(bi, EmiterX.intValue(), EmiterY.intValue(), 1, 1, pix, off, 1);
-                   
-                    }
-
-                }
-            }
-
-         
-
         } catch (IOException ex) {
             Logger.getLogger(T0M0GRAF.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-
-
 }
