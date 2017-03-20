2f9ef35cea222618fce5ae2c74af2e267c4988e3
Anna <anusia1199@o2.pl>
2017-03-20 16:28:10 +0100

    3

--- T0M0GRAF/src/t0m0graf/Sinogram.java
+++ T0M0GRAF/src/t0m0graf/Sinogram.java
@@ -15,15 +15,12 @@ import java.awt.image.DataBufferByte;
  */
 public class Sinogram {
 
-    Color[][] pix;//[360][n]
+    private Color[][] pix;//[360][n]
+    private BufferedImage buf;
 
-    public Color[][] getPix() {
-        return pix;
-    }
-
-    public void setPix(Color[][] pix) {
-        this.pix = pix;
-    }
+    private int emitersAmount;
+    private int detectorsAmount;
+    private int angle;
 
     /**
      *
@@ -31,25 +28,33 @@ public class Sinogram {
      * @param B rozwarość stożka
      * @param n liczba detektorow
      */
-    public Sinogram(Picture img, int B, int n) {
-        int r = (img.getBi().getWidth() / 2)-5;
+    public Sinogram(Picture img, int angle, int detecotrs, int emiters) {
+        this.angle = angle;
+        emitersAmount = emiters;
+        detectorsAmount = detecotrs;
+        buf = new BufferedImage(emitersAmount, detectorsAmount, BufferedImage.TYPE_BYTE_GRAY);
+        int r = (img.getBi().getWidth() / 2) - 5;
         BufferedImage bi = img.getBi();
-        pix = new Color[360][n];
+        pix = new Color[emitersAmount][detectorsAmount];
 
         //to trzeba dostosowac
-        for (int i = 0; i < 360; i++) {
+        for (int i = 0; i < emitersAmount; i++) {
             double help0 = i * Math.PI / 180;
             Double EmiterX = Math.cos(help0) * r + r;
             Double EmiterY = Math.sin(help0) * (-r) + r;
-            for (int j = 0; j < n; j++) {
-                double help = help0 + Math.PI - (B * Math.PI) / 360 + (B * Math.PI * j) / (180 * (n - 1));
+            for (int j = 0; j < detectorsAmount; j++) {
+                double help = help0 + Math.PI - (angle * Math.PI) / 360 + (angle * Math.PI * j) / (180 * (detectorsAmount - 1));
                 Double DetektorX = Math.cos(help) * r + r;
                 Double DetektorY = Math.sin(help) * (-r) + r;
                 //pix[i][j] 
-                System.out.println("r " + r + "   help  " + help);
-                System.out.println("ex " + EmiterX.intValue() + " ey " + EmiterY.intValue() + " dx " + DetektorX.intValue() + " dy " + DetektorY.intValue());
-                pix[i][j]  = BresenhamLine(EmiterX.intValue(), EmiterY.intValue(), DetektorX.intValue(), DetektorY.intValue(), bi);
+                //    System.out.println("r " + r + "   help  " + help);
+                // System.out.println("ex " + EmiterX.intValue() + " ey " + EmiterY.intValue() + " dx " + DetektorX.intValue() + " dy " + DetektorY.intValue());
+
+                pix[i][j] = BresenhamLine(EmiterX.intValue(), EmiterY.intValue(), DetektorX.intValue(), DetektorY.intValue(), bi);
+                // System.out.println("x  " + i + "   j   " + j + "  rgb   " + pix[i][j]);
                 //  PixelGrabber pixelGrab = new PixelGrabber(bi, EmiterX.intValue(), EmiterY.intValue(), 1, 1, pix, off, 1);
+                // setRGB(pix[i][j].getRed(),pix[i][j].getGreen(),pix[i][j].getBlue());
+                buf.setRGB(i, j, pix[i][j].getRGB());
             }
         }
     }
@@ -81,24 +86,23 @@ public class Sinogram {
         // pierwszy piksel
         //zczytuje kolor piksela 
         /* Color mycolor = new Color(image.getRGB(x, y));
-        suma += mycolor.getRed();
-        licznik++;
+         suma += mycolor.getRed();
+         licznik++;
          */
         //   byte[] pixels = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
         // int      argb = getRGB(x, y);
         // argb = (((int) pixels[3] & 0xff) << 16); // red
 
         /*
-       int[][] data = null ;
-image.getData().getPixels(0,0,image.getWidth(),image.getHeight(),data[x]);   
+         int[][] data = null ;
+         image.getData().getPixels(0,0,image.getWidth(),image.getHeight(),data[x]);   
          */
-       // System.out.println("x " + x + " y " + y);
-
+        // System.out.println("x " + x + " y " + y);
         int pixels = image.getRGB(x, y);
         int red = (pixels & 0x00ff0000) >> 16;
         //System.out.println(red);
         suma += red;
-       // System.out.println(suma);
+        // System.out.println(suma);
 
         licznik++;
 
@@ -124,17 +128,16 @@ image.getData().getPixels(0,0,image.getWidth(),image.getHeight(),data[x]);
                 //Color collll = new Color(pixels);
                 //  pixels = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
                 /* argb = getRGB(x, y);
-              //  argb = (((int) pixels[3] & 0xff) << 16); // red
-                suma += argb;
-                    System.out.println(argb);
+                 //  argb = (((int) pixels[3] & 0xff) << 16); // red
+                 suma += argb;
+                 System.out.println(argb);
                  */
-              // System.out.println("x " + x + " y " + y);
-
+                // System.out.println("x " + x + " y " + y);
                 pixels = image.getRGB(x, y);
                 red = (pixels & 0x00ff0000) >> 16;
-              //  System.out.println(red);
+                //  System.out.println(red);
                 suma += red;
-              //  System.out.println(suma);
+                //  System.out.println(suma);
 
                 licznik++;
             }
@@ -159,47 +162,62 @@ image.getData().getPixels(0,0,image.getWidth(),image.getHeight(),data[x]);
 
                 //   pixels = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
                 /*   argb = getRGB(x, y);
-               // argb = (((int) pixels[3] & 0xff) << 16); // red
-                suma += argb;
-                System.out.println(argb);
+                 // argb = (((int) pixels[3] & 0xff) << 16); // red
+                 suma += argb;
+                 System.out.println(argb);
                  */
-              //  System.out.println("x " + x + " y " + y);
+                //  System.out.println("x " + x + " y " + y);
                 pixels = image.getRGB(x, y);
 
                 red = (pixels & 0x00ff0000) >> 16;
-              //  System.out.println("red" + red);
+                //  System.out.println("red" + red);
                 suma += red;
 
-              //  System.out.println(suma);
-
+                //  System.out.println(suma);
                 licznik++;
             }
         }
         int kol = suma / licznik;
-        System.out.println("koniec");
-        System.out.println(suma);
-        System.out.println(licznik);
-        System.out.println(kol);
+        //  System.out.println("koniec");
+        // System.out.println(suma);
+        // System.out.println(licznik);
+        //  System.out.println(kol);
         return new Color(kol, kol, kol);
     }
 
-    private int width;
-    private int height;
-    private boolean hasAlphaChannel;
-    private int pixelLength;
-    private byte[] pixels;
+    public int getEmitersAmount() {
+        return emitersAmount;
+    }
 
-    int getRGB(int x, int y) {
-        int pos = (y * pixelLength * width) + (x * pixelLength);
+    public void setEmitersAmount(int emitersAmount) {
+        this.emitersAmount = emitersAmount;
+    }
 
-        int argb = -16777216; // 255 alpha
-        if (hasAlphaChannel) {
-            argb = (((int) pixels[pos++] & 0xff) << 24); // alpha
-        }
+    public int getDetectorsAmount() {
+        return detectorsAmount;
+    }
 
-        //  argb += ((int) pixels[pos++] & 0xff); // blue
-        // argb += (((int) pixels[pos++] & 0xff) << 8); // green
-        argb += (((int) pixels[pos++] & 0xff) << 16); // red
-        return argb;
+    public void setDetectorsAmount(int detectorsAmount) {
+        this.detectorsAmount = detectorsAmount;
+    }
+
+    public BufferedImage getBuf() {
+        return buf;
+    }
+
+    public Color[][] getPix() {
+        return pix;
+    }
+
+    public void setPix(Color[][] pix) {
+        this.pix = pix;
+    }
+
+    public int getAngle() {
+        return angle;
+    }
+
+    public void setAngle(int angle) {
+        this.angle = angle;
     }
 }
--- T0M0GRAF/src/t0m0graf/T0M0GRAF.java
+++ T0M0GRAF/src/t0m0graf/T0M0GRAF.java
@@ -5,12 +5,22 @@
  */
 package t0m0graf;
 
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics2D;
+import java.awt.Point;
+import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
+import java.awt.image.ColorModel;
+import java.awt.image.DataBufferInt;
+import java.awt.image.Raster;
+import java.awt.image.SampleModel;
+import java.awt.image.WritableRaster;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
+import java.util.Hashtable;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.stream.Stream;
@@ -21,6 +31,7 @@ import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import sun.awt.image.WritableRasterNative;
 
 /**
  *
@@ -33,9 +44,8 @@ public class T0M0GRAF extends JPanel {
      */
     public static JFrame frame;
     public static JLabel label1 = new JLabel();
+    public static JLabel label2 = new JLabel();
 
-    
-    
     /**
      * @param args the command line arguments
      */
@@ -44,8 +54,28 @@ public class T0M0GRAF extends JPanel {
         Picture picture = new Picture();
         ImageIcon imgIcon = new ImageIcon(picture.getBi());
         label1.setIcon(imgIcon);
-        Sinogram sinogram = new Sinogram(picture, 90, 20);
+        Sinogram sinogram = new Sinogram(picture, 180, 200,1000);
+        
+        TomographyPicture tomografPic=new TomographyPicture(picture,sinogram);
+        //  BufferedImage buffi=new BufferedImage(360, 20, BufferedImage.TYPE_BYTE_GRAY); 
+        //raster i dla kazdego piksela zapisywac]
+
+//Hashtable has=new Hashtable();
+        //  BufferedImage buffi=new BufferedImage(sinogram.getPix(), sinogram.getPix(), false,has);
+        /*
+             BufferedImage img = new BufferedImage(picture.getBi().getWidth(), picture.getBi().getHeight(), BufferedImage.TRANSLUCENT);
+    Graphics2D graphics = img.createGraphics(); */
+        //  Color newColor = new Color(sinogram.getPix(), sinogram.getPix(), sinogram.getPix(), 0 /* alpha needs to be zero */);
+        /* graphics.setXORMode(newColor);
+    graphics.drawImage(loadImg, null, 0, 0);
+    graphics.dispose();
+         */
+        // buffi.setData((Raster)sinogram.getPix());
+      //  ImageIcon imgIcon2 = new ImageIcon(sinogram.getBuf());
+       // label2.setIcon(imgIcon2);
         // frame.add(Box.createRigidArea(new Dimension(0, 5)));
+        ImageIcon imgIcon3 = new ImageIcon(tomografPic.getBuf());
+        label3.setIcon(imgIcon3);
 
     }
 
@@ -55,7 +85,8 @@ public class T0M0GRAF extends JPanel {
     public T0M0GRAF() {
         frame = new JFrame("Tomograf");
         frame.setSize(600, 600);
-        frame.add(label1);
+        // frame.add(label1);
+        frame.add(label3);
         frame.setVisible(true);
     }
 
